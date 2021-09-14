d82bcc4b61b6120ed39cbc20711e2c2f0a98ffe7
Danny Cobb <danieljcobbjr@gmail.com>
2021-09-13 22:39:55 -0500

    added Definitions for dynamic array class

---
+++ lists.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void listInit (list *n, int initSize){
+    n->used = 0;
+    n->size = initSize;
+    n->notes = malloc(initSize * sizeof(notes));
+}
+
+void addNote (list *n, notes element){
+    if (n->used == n->size){
+        n->size *= 2;
+        n->notes = realloc(n->notes, n->size * sizeof(notes));
+    }
+    n->notes[n->used++] = element;
+}
+
+void removeNote (list *n){
+    notes empty;
+    n->notes[n->used--] = empty;
+}
